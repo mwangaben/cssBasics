fac460e58c404b9828cb66a62fb58f820727088d
Benedict Mwanga <mwangaben@gmail.com>
2018-07-25 04:42:00 +0300

    Background with linear-gradient

--- index.php
+++ index.php
@@ -1,14 +1,24 @@
-<!DOCTYPE html>
-<html lang="en">
+<!DOCTYPE HTML>
+<html>
+	<head>
+		<!--
+			This file was provided as part of a weekly web design tutorial series.
+			Sign-up for our newsletter, at http://codifydesign.com, to be notified
+			about each new episode of CSS Shorts
+		-->
+		<meta http-equiv="Content-Type" content="text/html" charset="UTF-8">
+		<meta name="viewport" content="width=device-width, initial-scale=1">
+		<title>CSS Tutorials </title>
+		<link type="text/css" rel="stylesheet" href="css/app.css">
+	</head>
+	<body>
 
-<head>
-    <meta charset="UTF-8">
-    <title>Css Basics</title>
-    <link rel="stylesheet" href="css/app.css"> </head>
+		<div id="container">
 
-<body>
-    <p id="para"> Lorem ipsum dolor sit amet, consectetur adipisicing elit. Distinctio voluptatum architecto beatae, nobis veniam labore alias expedita repellendus eos cupiditate magnam tempora, odio sint asperiores, eum! Doloremque enim soluta, eum. </p>
-    <p> Lorem ipsum dolor sit amet, consectetur adipisicing elit. Ratione incidunt, architecto voluptate voluptatem unde similique tempore fugit nisi cum corporis provident fuga beatae omnis delectus esse veritatis nemo. Optio, eos. </p>
-</body>
+			<!-- Content source: Wikipedia -->
+			<p>The Louvre or the Louvre Museum is the world's largest museum and a historic monument in Paris, France. A central landmark of the city, it is located on the Right Bank of the Seine in the city's 1st arrondissement.</p>
 
+		</div>
+
+	</body>
 </html>
